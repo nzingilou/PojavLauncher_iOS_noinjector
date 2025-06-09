f37e61998596b223ab8c0f3b577c2fce8e4f86e8
nzingilou <mira.ditt@gmail.com>
2025-06-09 14:05:25 +0200

    Update JavaLauncher.m

--- Natives/JavaLauncher.m
+++ Natives/JavaLauncher.m
@@ -201,7 +201,7 @@ int launchJVM(NSString *username, id launchTarget, int width, int height, int mi
     NSString *librariesPath = [NSString stringWithFormat:@"%@/libs", NSBundle.mainBundle.bundlePath];
     margv[++margc] = [NSString stringWithFormat:@"-javaagent:%@/patchjna_agent.jar=", librariesPath].UTF8String;
     if(getPrefBool(@"general.cosmetica")) {
-        margv[++margc] = [NSString stringWithFormat:@"-javaagent:%@/arc_dns_injector.jar=23.95.137.176", librariesPath].UTF8String;
+//        margv[++margc] = [NSString stringWithFormat:@"-javaagent:%@/arc_dns_injector.jar=23.95.137.176", librariesPath].UTF8String;
     }
 
     // Workaround random stack guard allocation crashes
